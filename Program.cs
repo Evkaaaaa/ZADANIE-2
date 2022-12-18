97968c8ed13af92d832c4a8b62609a375f3e7671
Evkaaaaa <119289626+Evkaaaaa@users.noreply.github.com>
2022-12-18 20:35:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ЗАДАНИЕ_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово ");
+            string str = Console.ReadLine();
+            StringBuilder strb = new StringBuilder(str);
+
+            strb[0] = str[str.Length - 1];
+            strb[str.Length - 1] = str[0];
+            return strb.ToString();
+        }
+    }
+}
